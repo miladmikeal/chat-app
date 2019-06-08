8a7f6d2a022f102836b667ca8a2677c0a4757105
Milad Mikeal <miladmikeal@gmail.com>
2019-06-07 22:21:15 -0700

    Configure socket.io

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+io.on('connection', () => {
+  console.log('New WebSocket connection');
+});
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server started on port ${port}!`);
 });
