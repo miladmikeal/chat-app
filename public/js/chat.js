6bcf371a600429c5b5f53b26d255f41d19ba2564
Milad Mikeal <miladmikeal@gmail.com>
2019-06-08 00:41:58 -0700

    Add event acknowledgements

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,5 +11,29 @@ form.addEventListener('submit', (e) => {
 
   const message = e.target.elements.message.value;
 
-  socket.emit('sendMessage', message);
+  socket.emit('sendMessage', message, (error) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log('Message delivered.');
+  });
+})
+
+document.querySelector('#sendLocation').addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser.');
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const location = {
+      latitude,
+      longitude
+    }
+    socket.emit('sendLocation', location, () => {
+      console.log('Location shared.');
+    });
+  })
 })
