e54acc93eeedd2df02dc8210a50aadc13ee4e40c
Milad Mikeal <miladmikeal@gmail.com>
2019-06-09 19:15:17 -0700

    Adjust form and button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,14 +4,23 @@ socket.on('message', (message) => {
   console.log(message)
 })
 
-const form = document.querySelector('#messageForm');
+const $messageForm = document.querySelector('#messageForm');
+const $messageInput = $messageForm.querySelector('#messageInput');
+const $messageFormButton = $messageForm.querySelector('#send');
+const $sendLocationButton = document.querySelector('#sendLocation');
 
-form.addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  $messageFormButton.setAttribute('disabled', 'disabled');
 
   const message = e.target.elements.message.value;
 
   socket.emit('sendMessage', message, (error) => {
+    $messageFormButton.removeAttribute('disabled');
+    $messageInput.value = '';
+    $messageInput.focus();
+
     if (error) {
       return console.log(error);
     }
@@ -20,10 +29,12 @@ form.addEventListener('submit', (e) => {
   });
 })
 
-document.querySelector('#sendLocation').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
   if (!navigator.geolocation) {
     return alert('Geolocation is not supported by your browser.');
   }
+
+  $sendLocationButton.setAttribute('disabled', 'disabled');
 
   navigator.geolocation.getCurrentPosition((position) => {
     const latitude = position.coords.latitude;
@@ -33,6 +44,7 @@ document.querySelector('#sendLocation').addEventListener('click', () => {
       longitude
     }
     socket.emit('sendLocation', location, () => {
+      $sendLocationButton.removeAttribute('disabled');
       console.log('Location shared.');
     });
   })
